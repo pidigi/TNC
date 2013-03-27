9628cc3842c52a81ebb8a0a9da94e8e9214b0abd
Extrimius <pieter.lietaert@gmail.com>
2013-03-27 22:25:59 +0100

    AsteroidRock_to_Asteroid

---
+++ Asteroids/src/asteroids/model/Asteroid.java
@@ -0,0 +1,18 @@
+package asteroids.model;
+
+import be.kuleuven.cs.som.annotate.Raw;
+
+/**
+ * A class of ships for the game asteroids.
+ * 
+ * @version  1.0
+ * @author   Frederik Van Eeghem (1st master Mathematical engineering), 
+ *		 	 Pieter Lietaert (1st master Mathematical engineering)
+ */
+public class Asteroid extends SpacialElement{
+	@Raw
+	public AsteroidRock(Vector2D position, double radius, Vector2D velocity, double maxSpeed, double mass)
+			throws IllegalArgumentException, NullPointerException{
+		super(position,radius,velocity,maxSpeed,mass);
+	}
+}
